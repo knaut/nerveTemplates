6b8638a549f20733b7b59ccdcfd6c5a010803185
knaut <info@knaut.net>
2015-10-15 20:42:22 -0700

    removed occurance

--- parser3.js
+++ parser3.js
@@ -1529,28 +1529,6 @@ var escapeChar = function(match) {
 	return '\\' + escapes[match];
 };
 
-
-/** Function count the occurrences of substring in a string;
- * @param {String} string   Required. The string;
- * @param {String} subString    Required. The string to search for;
- * @param {Boolean} allowOverlapping    Optional. Default: false;
- * @author Vitim.us http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
- */
-function occurrences(string, subString, allowOverlapping){
-
-    string+=""; subString+="";
-    if(subString.length<=0) return string.length+1;
-
-    var n=0, pos=0;
-    var step=allowOverlapping?1:subString.length;
-
-    while(true){
-        pos=string.indexOf(subString,pos);
-        if(pos>=0){ ++n; pos+=step; } else break;
-    }
-    return n;
-}
-
 normalizeFunction = function( func ) {
 	rgfuncHead = /^function\s*[(][)]\s*[{]\s*/;	// eg 'function() { …'
 	rgfuncTail = /\s*[}]{1}\s*$/;		// eg last } bracket of the function
