b478077e25a0d8568eabc696c12dbf0864b78994
_knaut <info@knaut.net>
2015-12-28 00:05:53 -0800

    poor attempts at modularization of a recursive function

--- modules/stringify.js
+++ modules/stringify.js
@@ -1,6 +1,8 @@
 var stringify = (function() {
 
 	return {
+		string: '',
+
 		normalizedFunction: function( obj ) {
 
 			var splitSrc = obj.src.split('%break%');
@@ -13,9 +15,47 @@ var stringify = (function() {
 			var innerString;
 			var stringifiedInnerSrc;
 
+			console.log(obj)
 			for (var i = 0; splitSrc.length > i; i++) {
-				console.log( splitSrc[i] )
+				console.log( splitSrc[i], i )
+				console.log( obj.inner[i] );
+
+				if (obj.inner[i] !== undefined) {
+					console.log( this.normalizedHtml( obj.inner[i] ) );	
+				}
+				
 			}
+		},
+
+		normalizedHtml: function( obj ) {
+			var string = '';
+
+			string += '<' + obj.tagName;
+
+			// id
+			if (obj.id) {
+				string += ' id="' + obj.id + '"';
+			}
+
+			// classes
+			if (obj.hasOwnProperty('classes') && obj.classes.length) {
+				string += ' class="' + obj.classes.join(' ') + '"';
+			}
+
+			// custom attributes
+			if (obj.hasOwnProperty('attrs') && obj.attrs.length) {
+				// loop through array of key/vals
+				for (var a = 0; obj.attrs.length > a; a++) {
+					var attr = obj.attrs[a];
+					for (var attrKey in attr) {
+						string += ' ' + attrKey + '="' + attr[attrKey] + '"'
+					}
+				}
+			}
+
+			// end the tag
+			string += '>';
+			console.log(string)
 		}
 	}
 
